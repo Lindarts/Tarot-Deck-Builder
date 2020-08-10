1b70f922eb5f3c1531d118d839b885d84f552bc5
Lind <davide.mariani.94@hotmail.it>
2020-08-10 17:34:18 +0200

    Intro database

---
+++ Tarot/Assets/ScriptableObject/Cards/Scripts/CardObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardType{
+    Mine,
+    Blessing,
+    Relic,
+    Ritual,
+    Trick,
+    Unit
+}
+public abstract class CardObject : ScriptableObject {
+    public GameObject prefab;
+    public CardType type;
+    [TextArea(10,15)]
+    public string description;
+}
---
+++ Tarot/Assets/ScriptableObject/Cards/Scripts/UnitObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Unit Object", menuName = "Card System/Cards/Unit")]
+public class UnitObject : CardObject
+{
+    public void awake(){
+        type = CardType.Unit;
+    }
+}
